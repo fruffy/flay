bb80c9c61543fc5ae199320bf1c1dfba8bca2890
fruffy <fruffy@nyu.edu>
2023-11-25 23:04:18 -0500

    Build fixes.

--- cmake/GRpc.cmake
+++ cmake/GRpc.cmake
@@ -3,6 +3,9 @@ macro(p4c_obtain_grpc)
 # simple way to add this awful dependency.
 set(FETCHCONTENT_QUIET_PREV ${FETCHCONTENT_QUIET})
 set(FETCHCONTENT_QUIET OFF)
+set(CMAKE_FIND_LIBRARY_SUFFIXES_PREV ${CMAKE_FIND_LIBRARY_SUFFIXES_PREV})
+set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
+
 set(gRPC_BUILD_GRPC_CPP_PLUGIN ON CACHE BOOL "Build grpc_cpp_plugin")
 set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE BOOL "Build grpc_csharp_plugin")
 set(gRPC_BUILD_CSHARP_EXT OFF CACHE BOOL "Build C# extensions")
@@ -13,7 +16,7 @@ set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE BOOL "Build grpc_python_plugin")
 set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE BOOL "Build grpc_ruby_plugin")
 set(gRPC_ZLIB_PROVIDER "package" CACHE STRING "Provider of zlib library")
 set(gRPC_CARES_PROVIDER "package" CACHE STRING "Provider of c-ares library")
-set(gRPC_RE2_PROVIDER "package" CACHE STRING "Provider of re2 library")
+set(gRPC_RE2_PROVIDER "module" CACHE STRING "Provider of re2 library")
 set(gRPC_SSL_PROVIDER "package" CACHE STRING "Provider of ssl library")
 set(gRPC_PROTOBUF_PROVIDER "package" CACHE STRING "Provider of protobuf library")
 set(gRPC_PROTOBUF_PACKAGE_TYPE "MODULE" CACHE STRING "Algorithm for searching protobuf package")
@@ -38,6 +41,7 @@ set(FETCHCONTENT_QUIET ${FETCHCONTENT_QUIET_PREV})
 
 set(GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
 set(GRPC_INCLUDE_DIR "${grpc_SOURCE_DIR}/include")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_PREV})
 
 endmacro(p4c_obtain_grpc)
 
