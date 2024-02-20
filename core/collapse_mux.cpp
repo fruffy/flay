7432437f1f5857bd711bb9d30c1a0c51270b97cf
fruffy <fruffy@nyu.edu>
2024-02-20 10:21:44 -0500

    Cleanup.

--- core/collapse_mux.cpp
+++ core/collapse_mux.cpp
@@ -10,114 +10,103 @@
 
 namespace P4Tools {
 
-bool MuxCondComp::operator()(const IR::Expression *s1, const IR::Expression *s2) const {
-    return s1->isSemanticallyLess(*s2);
-}
+class FoldMuxConditionDown : public Transform {
+ private:
+    using ExpressionMap = std::map<const IR::Expression *, bool, IR::IsSemanticallyLessComparator>;
 
-std::optional<bool> optionalValue(const ConditionMap &conditionMap, const IR::Expression *cond) {
-    if (const auto *boolLiteral = cond->to<IR::BoolLiteral>()) {
-        return boolLiteral->value;
-    }
-    auto it = conditionMap.find(cond);
-    if (it != conditionMap.end()) {
-        return it->second;
-    }
-    return std::nullopt;
-}
-
-const IR::Node *CollapseMux::preorder(IR::Mux *mux) {
-    // TODO: We should not use this prune but it is needed to avoid an infinite loop for an unknown
-    // reason.
-    prune();
-    const auto *cond = mux->e0;
-    auto condIt = conditionMap.find(cond);
-    if (condIt != conditionMap.end()) {
-        if (condIt->second) {
-            return mux->e1->apply(CollapseExpression(conditionMap));
+    static std::optional<bool> optionalValue(const ExpressionMap &expressionMap,
+                                             const IR::Expression *cond) {
+        if (const auto *boolLiteral = cond->to<IR::BoolLiteral>()) {
+            return boolLiteral->value;
         }
-        return mux->e2->apply(CollapseExpression(conditionMap));
-    }
-    mux->e0 = mux->e0->apply(CollapseExpression(conditionMap));
-    auto conditionMapE1 = conditionMap;
-    conditionMapE1.emplace(cond, true);
-    mux->e1 = mux->e1->apply(CollapseExpression(conditionMapE1));
-    auto conditionMapE2 = conditionMap;
-    conditionMapE2.emplace(cond, false);
-    mux->e2 = mux->e2->apply(CollapseExpression(conditionMapE2));
-    return mux;
-}
-
-const IR::Node *CollapseMux::postorder(IR::Expression *expr) {
-    auto it = conditionMap.find(expr);
-    if (it != conditionMap.end()) {
-        return IR::getBoolLiteral(it->second);
-    }
-    return expr;
-}
-
-const IR::Node *CollapseExpression::postorder(IR::Expression *expr) {
-    auto it = conditionMap.find(expr);
-    if (it != conditionMap.end()) {
-        return IR::getBoolLiteral(it->second);
-    }
-    return expr;
-}
-
-const IR::Node *CollapseExpression::postorder(IR::LAnd *expr) {
-    auto leftOpt = optionalValue(conditionMap, expr->left);
-    if (leftOpt.has_value()) {
-        if (leftOpt.value()) {
-            auto rightOpt = optionalValue(conditionMap, expr->right);
-            return rightOpt.has_value() ? IR::getBoolLiteral(rightOpt.value()) : expr->right;
+        auto it = expressionMap.find(cond);
+        if (it != expressionMap.end()) {
+            return it->second;
         }
-        return IR::getBoolLiteral(false);
+        return std::nullopt;
     }
-    auto rightOpt = optionalValue(conditionMap, expr->right);
-    if (rightOpt.has_value()) {
-        return rightOpt.value() ? expr->left : IR::getBoolLiteral(false);
-    }
-    return expr;
-}
 
-const IR::Node *CollapseExpression::postorder(IR::LOr *expr) {
-    auto leftOpt = optionalValue(conditionMap, expr->left);
-    if (leftOpt.has_value()) {
-        if (leftOpt.value()) {
-            return IR::getBoolLiteral(true);
+    class FoldExpression : public Transform {
+     private:
+        ExpressionMap expressionMap;
+
+     public:
+        explicit FoldExpression(ExpressionMap expressionMap)
+            : expressionMap(std::move(expressionMap)) {}
+        FoldExpression() = default;
+
+        const IR::Node *postorder(IR::LAnd *expr) override {
+            auto leftOpt = optionalValue(expressionMap, expr->left);
+            if (leftOpt.has_value()) {
+                if (leftOpt.value()) {
+                    auto rightOpt = optionalValue(expressionMap, expr->right);
+                    return rightOpt.has_value() ? IR::getBoolLiteral(rightOpt.value())
+                                                : expr->right;
+                }
+                return IR::getBoolLiteral(false);
+            }
+            auto rightOpt = optionalValue(expressionMap, expr->right);
+            if (rightOpt.has_value()) {
+                return rightOpt.value() ? expr->left : IR::getBoolLiteral(false);
+            }
+            return expr;
         }
-        auto rightOpt = optionalValue(conditionMap, expr->right);
-        return rightOpt.has_value() ? IR::getBoolLiteral(rightOpt.value()) : expr->right;
-    }
-    auto rightOpt = optionalValue(conditionMap, expr->right);
-    if (rightOpt.has_value()) {
-        return rightOpt.value() ? IR::getBoolLiteral(true) : expr->left;
-    }
-    return expr;
-}
 
-const IR::Node *CollapseExpression::postorder(IR::LNot *expr) {
-    auto exprIt = conditionMap.find(expr->expr);
-    if (exprIt != conditionMap.end()) {
-        return IR::getBoolLiteral(!exprIt->second);
+        const IR::Node *postorder(IR::LOr *expr) override {
+            auto leftOpt = optionalValue(expressionMap, expr->left);
+            if (leftOpt.has_value()) {
+                if (leftOpt.value()) {
+                    return IR::getBoolLiteral(true);
+                }
+                auto rightOpt = optionalValue(expressionMap, expr->right);
+                return rightOpt.has_value() ? IR::getBoolLiteral(rightOpt.value()) : expr->right;
+            }
+            auto rightOpt = optionalValue(expressionMap, expr->right);
+            if (rightOpt.has_value()) {
+                return rightOpt.value() ? IR::getBoolLiteral(true) : expr->left;
+            }
+            return expr;
+        }
+
+        const IR::Node *postorder(IR::LNot *expr) override {
+            auto exprIt = expressionMap.find(expr->expr);
+            if (exprIt != expressionMap.end()) {
+                return IR::getBoolLiteral(!exprIt->second);
+            }
+
+            return expr;
+        }
+
+        const IR::Node *postorder(IR::Mux *mux) override {
+            auto condOpt = optionalValue(expressionMap, mux->e0);
+            if (condOpt.has_value()) {
+                return condOpt.value() ? mux->e1 : mux->e2;
+            }
+            if (mux->e1->equiv(*mux->e2)) {
+                return mux->e1;
+            }
+            return mux;
+        }
+    };
+
+    const IR::Node *preorder(IR::Mux *mux) override {
+        // TODO: We should not use this prune but it is needed to avoid an infinite loop for an
+        // unknown reason.
+        prune();
+        const auto *cond = mux->e0;
+        mux->e0 = mux->e0->apply(FoldExpression());
+        mux->e1 = mux->e1->apply(FoldExpression({{cond, true}}));
+        mux->e2 = mux->e2->apply(FoldExpression({{cond, false}}));
+        return mux;
     }
 
-    return expr;
-}
-
-const IR::Node *CollapseExpression::postorder(IR::Mux *mux) {
-    auto condOpt = optionalValue(conditionMap, mux->e0);
-    if (condOpt.has_value()) {
-        return condOpt.value() ? mux->e1 : mux->e2;
-    }
-    if (mux->e1->equiv(*mux->e2)) {
-        return mux->e1;
-    }
-    return mux;
-}
-
-class MuxFolder : public Transform {
  public:
-    MuxFolder() = default;
+    FoldMuxConditionDown() { visitDagOnce = false; }
+};
+
+class LiftMuxConditions : public Transform {
+ public:
+    LiftMuxConditions() = default;
 
     const IR::Node *preorder(IR::Mux *mux) override {
         if (const auto *trueMux = mux->e1->to<IR::Mux>()) {
@@ -151,30 +140,29 @@ class MuxFolder : public Transform {
     }
 };
 
-const IR::Expression *CollapseMux::produceOptimizedMux(const IR::Expression *cond,
-                                                       const IR::Expression *trueExpression,
-                                                       const IR::Expression *falseExpression) {
+namespace CollapseMux {
+
+const IR::Expression *produceOptimizedMux(const IR::Expression *cond,
+                                          const IR::Expression *trueExpression,
+                                          const IR::Expression *falseExpression) {
     Util::ScopedTimer timer("Mux optimization");
     auto *mux = new IR::Mux(trueExpression->type, cond, trueExpression, falseExpression);
-    return optimizeExpression(mux->apply(CollapseMux()));
+    return optimizeExpression(mux->apply(FoldMuxConditionDown()));
 }
 
-const IR::Expression *CollapseMux::optimizeExpression(const IR::Expression *expr) {
+const IR::Expression *optimizeExpression(const IR::Expression *expr) {
     // Lifted from frontends/p4/optimizeExpressions.
     auto pass = PassRepeated({
         new P4::ConstantFolding(nullptr, nullptr, false),
         new P4::StrengthReduction(nullptr, nullptr, nullptr),
-        new CollapseMux(),
-        new MuxFolder(),
+        new FoldMuxConditionDown(),
+        new LiftMuxConditions(),
     });
     expr = expr->apply(pass);
     BUG_CHECK(::errorCount() == 0, "Encountered errors while trying to optimize expressions.");
     return expr;
 }
 
-CollapseMux::CollapseMux(const std::map<const IR::Expression *, bool, MuxCondComp> &conditionMap)
-    : conditionMap(conditionMap) {
-    visitDagOnce = false;
-}
+}  // namespace CollapseMux
 
 }  // namespace P4Tools
--- core/collapse_mux.h
+++ core/collapse_mux.h
@@ -1,56 +1,20 @@
 #ifndef BACKENDS_P4TOOLS_MODULES_FLAY_CORE_COLLAPSE_MUX_H_
 #define BACKENDS_P4TOOLS_MODULES_FLAY_CORE_COLLAPSE_MUX_H_
 
-#include <map>
-
 #include "ir/ir.h"
-#include "ir/node.h"
-#include "ir/visitor.h"
 
 namespace P4Tools {
 
-struct MuxCondComp {
-    bool operator()(const IR::Expression *s1, const IR::Expression *s2) const;
-};
+namespace CollapseMux {
 
-using ConditionMap = std::map<const IR::Expression *, bool, MuxCondComp>;
+/// Produce a Mux expression where the amount of sub-expressions has been minimized.
+const IR::Expression *produceOptimizedMux(const IR::Expression *cond,
+                                          const IR::Expression *trueExpression,
+                                          const IR::Expression *falseExpression);
 
-class CollapseExpression : public Transform {
- private:
-    ConditionMap conditionMap;
+const IR::Expression *optimizeExpression(const IR::Expression *expr);
 
- public:
-    explicit CollapseExpression(const ConditionMap &conditionMap) : conditionMap(conditionMap) {}
-    CollapseExpression() = default;
-
-    const IR::Node *postorder(IR::Expression *expr) override;
-    const IR::Node *postorder(IR::LAnd *expr) override;
-    const IR::Node *postorder(IR::LOr *expr) override;
-    const IR::Node *postorder(IR::LNot *expr) override;
-    const IR::Node *postorder(IR::Mux *mux) override;
-};
-
-class CollapseMux : public Transform {
- private:
-    ConditionMap conditionMap;
-
- public:
-    explicit CollapseMux(const ConditionMap &conditionMap);
-    CollapseMux() = default;
-
-    const IR::Node *preorder(IR::Mux *mux) override;
-    const IR::Node *postorder(IR::Expression *expr) override;
-    // const IR::Node *preorder(IR::LAnd *expr) override;
-    // const IR::Node *preorder(IR::LOr *expr) override;
-    // const IR::Node *preorder(IR::LNot *expr) override;
-
-    /// Produce a Mux expression where the amount of sub-expressions has been minimized.
-    static const IR::Expression *produceOptimizedMux(const IR::Expression *cond,
-                                                     const IR::Expression *trueExpression,
-                                                     const IR::Expression *falseExpression);
-
-    static const IR::Expression *optimizeExpression(const IR::Expression *expr);
-};
+};  // namespace CollapseMux
 
 }  // namespace P4Tools
 
