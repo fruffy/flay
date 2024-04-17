6a0ce55b1715b6b1c7d369b8a8f315491d6c9b72
fruffy <fruffy@nyu.edu>
2024-04-17 15:20:46 -0400

    Entry fix.

--- control_plane/symbolic_state.cpp
+++ control_plane/symbolic_state.cpp
@@ -119,6 +119,31 @@ std::optional<TableKeySet> ControlPlaneStateInitializer::computeEntryKeySet(
     return keySet;
 }
 
+std::optional<const IR::Expression *> computeEntryAction(
+    const IR::P4Table &table, const IR::P4Action &actionDecl,
+    const IR::MethodCallExpression &actionCall) {
+    const IR::Expression *actionConstraint =
+        new IR::Equ(ControlPlaneState::getTableActionChoice(table.controlPlaneName()),
+                    IR::getStringLiteral(actionDecl.controlPlaneName()));
+    RETURN_IF_FALSE_WITH_MESSAGE(
+        actionCall.arguments->size() == actionDecl.parameters->parameters.size(), std::nullopt,
+        ::error("Entry call %1% in table %2% does not have the right number of arguments.",
+                actionCall, table));
+    for (size_t idx = 0; idx < actionCall.arguments->size(); ++idx) {
+        const auto *argument = actionCall.arguments->at(idx);
+        const auto *parameter = actionDecl.parameters->parameters.at(idx);
+        const auto *actionVariable = ControlPlaneState::getTableActionArgument(
+            table.controlPlaneName(), actionDecl.controlPlaneName(), parameter->controlPlaneName(),
+            parameter->type);
+        RETURN_IF_FALSE_WITH_MESSAGE(
+            argument->expression->is<IR::Constant>(), std::nullopt,
+            ::error("Argument %1% in table %2% is not a constant.", argument, table));
+        actionConstraint =
+            new IR::LAnd(actionConstraint, new IR::Equ(actionVariable, argument->expression));
+    }
+    return actionConstraint;
+}
+
 std::optional<TableEntrySet> ControlPlaneStateInitializer::initializeTableEntries(
     const IR::P4Table *table, const P4::ReferenceMap &refMap) {
     TableEntrySet initialTableEntries;
@@ -126,21 +151,25 @@ std::optional<TableEntrySet> ControlPlaneStateInitializer::initializeTableEntrie
     RETURN_IF_FALSE(entries != nullptr, initialTableEntries);
 
     for (const auto *entry : entries->entries) {
-        const auto *action = entry->getAction();
+        const auto *actionCallExpression = entry->getAction();
         ASSIGN_OR_RETURN_WITH_MESSAGE(
-            const auto &actionCall, action->to<IR::MethodCallExpression>(), std::nullopt,
-            ::error("Action %1% in table %2% is not a method call.", action, table));
-        ASSIGN_OR_RETURN_WITH_MESSAGE(
-            const auto &methodName, actionCall.method->to<IR::PathExpression>(), std::nullopt,
-            ::error("Action %1% in table %2% is not a path expression.", action, table));
+            const auto &actionCall, actionCallExpression->to<IR::MethodCallExpression>(),
+            std::nullopt,
+            ::error("Action %1% in table %2% is not a method call.", actionCallExpression, table));
+        ASSIGN_OR_RETURN_WITH_MESSAGE(const auto &methodName,
+                                      actionCall.method->to<IR::PathExpression>(), std::nullopt,
+                                      ::error("Action %1% in table %2% is not a path expression.",
+                                              actionCallExpression, table));
         ASSIGN_OR_RETURN_WITH_MESSAGE(
             auto &actionDecl, refMap.getDeclaration(methodName.path, false), std::nullopt,
             ::error("Action reference %1% not found in the reference map", methodName));
-        auto *actionAssignment =
-            new IR::Equ(ControlPlaneState::getTableActionChoice(table->controlPlaneName()),
-                        IR::getStringLiteral(actionDecl.controlPlaneName()));
+        ASSIGN_OR_RETURN_WITH_MESSAGE(auto &action, actionDecl.to<IR::P4Action>(), std::nullopt,
+                                      ::error("%1% is not a P4Action.", actionDecl));
+        ASSIGN_OR_RETURN(auto actionConstraint, computeEntryAction(*table, action, actionCall),
+                         std::nullopt);
         ASSIGN_OR_RETURN(auto entryKeySet, computeEntryKeySet(*table, *entry), std::nullopt);
         std::optional<int32_t> entryPriority;
+        // In some cases, there is no priority. Try to create a priority from the entry.
         if (entry->priority != nullptr) {
             ASSIGN_OR_RETURN_WITH_MESSAGE(auto entryPriorityConstant,
                                           entry->priority->to<IR::Constant>(), std::nullopt,
@@ -151,7 +180,7 @@ std::optional<TableEntrySet> ControlPlaneStateInitializer::initializeTableEntrie
             entryPriority = 0;
         }
         initialTableEntries.insert(
-            *new TableMatchEntry(actionAssignment, entryPriority.value(), entryKeySet));
+            *new TableMatchEntry(actionConstraint, entryPriority.value(), entryKeySet));
     }
     return initialTableEntries;
 }
