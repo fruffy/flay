cce15dd8713d10e9eb5992e5fcdc7d8b1e70bace
fruffy <fruffy@nyu.edu>
2024-04-29 15:19:54 -0400

    Fix execution of basic scion programs. Disable IPv4 for them.

--- targets/tofino/base/expression_resolver.cpp
+++ targets/tofino/base/expression_resolver.cpp
@@ -142,7 +142,7 @@ const ExternMethodImpls EXTERN_METHOD_IMPLS(
      /// Load the counter with an immediate value or a header field.
      {"ParserCounter.set",
       {"value"},
-      [](const ExternMethodImpls::ExternInfo &externInfo) { return nullptr; }},
+      [](const ExternMethodImpls::ExternInfo & /*externInfo*/) { return nullptr; }},
      /// Load the counter with a header field.
      /// @param max : Maximum permitted value for counter (pre rotate/mask/add).
      /// @param rotate : Right rotate (circular) the source field by this number of bits.
@@ -175,12 +175,12 @@ const ExternMethodImpls EXTERN_METHOD_IMPLS(
      /// @param value : Constant to add to the counter.
      {"ParserCounter.increment",
       {"value"},
-      [](const ExternMethodImpls::ExternInfo &externInfo) { return nullptr; }},
+      [](const ExternMethodImpls::ExternInfo & /*externInfo*/) { return nullptr; }},
      /// Subtract an immediate value from the parser counter.
      /// @param value : Constant to subtract from the counter.
      {"ParserCounter.decrement",
       {"value"},
-      [](const ExternMethodImpls::ExternInfo &externInfo) { return nullptr; }},
+      [](const ExternMethodImpls::ExternInfo & /*externInfo*/) { return nullptr; }},
 
      // -----------------------------------------------------------------------------
      // PARSER PRIORITY
