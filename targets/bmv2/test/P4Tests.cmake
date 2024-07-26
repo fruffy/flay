c45492f8d884e1a768f7b0c6abbab5492c1190e8
Fabian Ruffy <5960321+fruffy@users.noreply.github.com>
2024-07-26 19:44:57 +0200

    Add some changes for RTSmith. (#133)

--- targets/bmv2/test/P4Tests.cmake
+++ targets/bmv2/test/P4Tests.cmake
@@ -8,7 +8,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/TestTemplate.cmake)
 # ##################################################################################################
 set(V1_SEARCH_PATTERNS "include.*v1model.p4" "main|common_v1_test")
 # General BMv2 tests supplied by the compiler.
-set(P4TESTS_FOR_BMV2
+set(FLAY_P4TESTS_FOR_BMV2
     "${P4C_SOURCE_DIR}/testdata/p4_16_samples/*.p4"
     "${P4C_SOURCE_DIR}/testdata/p4_16_samples/dash/*.p4"
     "${P4C_SOURCE_DIR}/testdata/p4_16_samples/fabric_*/fabric.p4"
@@ -18,12 +18,12 @@ set(P4TESTS_FOR_BMV2
     "${CMAKE_CURRENT_LIST_DIR}/programs/*.p4"
 )
 
-p4c_find_tests("${P4TESTS_FOR_BMV2}" P4_16_V1_TESTS INCLUDE "${V1_SEARCH_PATTERNS}" EXCLUDE "")
+p4c_find_tests("${FLAY_P4TESTS_FOR_BMV2}" FLAY_P4_16_BMV2_V1_TESTS INCLUDE "${V1_SEARCH_PATTERNS}" EXCLUDE "")
 
 # Filter some programs  because they have issues that are not captured with Xfails.
 list(
   REMOVE_ITEM
-  P4_16_V1_TESTS
+  FLAY_P4_16_BMV2_V1_TESTS
   # These tests time out and require fixing.
 )
 
@@ -31,7 +31,7 @@ set (EXTRA_OPTS "--reference-folder ${CMAKE_CURRENT_LIST_DIR}/testdata")
 
 p4tools_add_tests(
   TESTS
-  "${P4_16_V1_TESTS}"
+  "${FLAY_P4_16_BMV2_V1_TESTS}"
   TAG
   "flay-bmv2-v1model"
   DRIVER
