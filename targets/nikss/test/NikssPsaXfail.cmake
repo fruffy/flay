aad23518c720d9558942a334f40cc39fa59f6f1c
fruffy <fruffy@nyu.edu>
2024-08-31 18:17:12 +0200

    Fix.

--- targets/nikss/test/NikssPsaXfail.cmake
+++ targets/nikss/test/NikssPsaXfail.cmake
@@ -4,12 +4,3 @@ p4tools_add_xfail_reason(
   "Compiler Bug|Unimplemented compiler support"
   register-structs.p4 # Unimplemented compiler support: Unsupported assignment type struct
 )
-
-p4tools_add_xfail_reason(
-  "flay-nikss-psa"
-  "unsupported argument expression"
-  # Issue with some control-plane code.
-  const-entry-and-action.p4
-  const-entry-ternary.p4
-  const-entry.p4
-)
